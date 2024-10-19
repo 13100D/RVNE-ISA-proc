5e7d4d34c63ce5c958485fbb2ec547368d678a60
riyasach189 <riyasach189@gmail.com>
2024-10-19 21:13:02 +0530

    adding vivado project

--- rvne.v
+++
@@ -1,4 +0,0 @@
-module rvne(input [31:0] instruction);
-    wire [31:0] [31:0] regFile;
-    
-endmodule
---
+++ rvne/rvne.srcs/sources_1/new/top.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.10.2024 21:00:21
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    output [31:0] wvr_status,
+    output [31:0] svr_status
+    );
+endmodule
